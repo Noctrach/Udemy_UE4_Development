fe2de608385878ea18aac1d23d59c5313436cc7e
Noctrach <14197969+Noctrach@users.noreply.github.com>
2018-08-10 13:10:56 +0200

    Removed these even though ignore?

--- Section_3/Building_Escape/Intermediate/Build/Win64/Building_Escape/Development/UELinkerFixups/PCH.UELinkerFixups.cpp
+++
@@ -1,1 +0,0 @@
-#include "C:/Users/Peter/MyDocs/Projects/Udemy_UE4_Development/Section_3/Building_Escape/Intermediate/Build/Win64/Building_Escape/Development/UELinkerFixups/PCH.UELinkerFixups.h"
--- Section_3/Building_Escape/Intermediate/Build/Win64/Building_Escape/Development/UELinkerFixups/PCH.UELinkerFixups.h
+++
@@ -1,2 +0,0 @@
-// PCH for C:\Users\Peter\MyDocs\Projects\Udemy_UE4_Development\Section_3\Building_Escape\Intermediate\Build\Win64\UE4\Development\UELinkerFixupsName.h
-#include "C:\Users\Peter\MyDocs\Projects\Udemy_UE4_Development\Section_3\Building_Escape\Intermediate\Build\Win64\UE4\Development\UELinkerFixupsName.h"
