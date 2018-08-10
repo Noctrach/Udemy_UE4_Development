d209f1016e9fb5c38bb9ce8b2d5c525cf93806a4
Noctrach <14197969+Noctrach@users.noreply.github.com>
2018-08-10 13:05:37 +0200

    Rebuild attempt desktop + gitignore test

---
+++ Section_3/Building_Escape/Intermediate/Build/Win64/Building_Escape/Development/UELinkerFixups/PCH.UELinkerFixups.cpp
@@ -0,0 +1,1 @@
+#include "C:/Users/Peter/MyDocs/Projects/Udemy_UE4_Development/Section_3/Building_Escape/Intermediate/Build/Win64/Building_Escape/Development/UELinkerFixups/PCH.UELinkerFixups.h"
---
+++ Section_3/Building_Escape/Intermediate/Build/Win64/Building_Escape/Development/UELinkerFixups/PCH.UELinkerFixups.h
@@ -0,0 +1,2 @@
+// PCH for C:\Users\Peter\MyDocs\Projects\Udemy_UE4_Development\Section_3\Building_Escape\Intermediate\Build\Win64\UE4\Development\UELinkerFixupsName.h
+#include "C:\Users\Peter\MyDocs\Projects\Udemy_UE4_Development\Section_3\Building_Escape\Intermediate\Build\Win64\UE4\Development\UELinkerFixupsName.h"
